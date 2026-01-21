82c1580a17e7575d297e6c08e4d3ac07f7e556ae
Harshi1689 <Harshi1689@gmail.com>
2026-01-21 19:02:13 +0530

    Import export in nodejs learning

---
+++ newfile.js
@@ -0,0 +1,7 @@
+export const sum=(a,b) =>a+b;
+export const mul=(a,b)=> a*b;
+export const g=9.5;
+
+
+//make sure that package.json file has { "type"="module"}before expoert
+// if not add it
---
+++ script.js
@@ -0,0 +1,5 @@
+import {sum,mul} from "./newfile.js"
+
+//using import e can select only single this or many from another file
+
+console.log(sum(10,12));
